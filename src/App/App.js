9efef1c1a73d40387d578e9435aa23794a6c0029
djwelsh98 <dustin_welsh98@yahoo.com>
2022-01-04 21:35:54 -0700

    useState

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 import SubmitForm from './components/SubmitForm';
-import FirstCard from './components/Card';
+import FirstCard from './components/FirstCard';
 
 function App() {
+
+  const [ book, setBook ] = useState({title: "name", author: "person", synopsis: "stuff"});
+
+  const handleChange = (title, author, synopsis) => {
+    console.log(title)
+    setBook({title, author, synopsis})
+    // (setBook(oldBook => [ ...oldBook, {title, author, synopsis}]))
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-         <SubmitForm/>
+         <SubmitForm handleChange={handleChange}/>
       </header>
-     <FirstCard/>
+     <FirstCard book={book}/>
     </div>
   );
 }
--- src/App/components/SubmitForm.js
+++ src/App/components/SubmitForm.js
@@ -1,21 +1,30 @@
 import { Form, Button } from 'react-bootstrap';
 
-function SubmitForm() {
+
+
+
+const SubmitForm =( {handleChange}) => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        handleChange(
+            e.target.title.value, e.target.author.value, e.target.synopsis.value
+        )
+    }
     return (
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicTitle">
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="title" placeholder="To Kill A Mockingbird" />
+                <Form.Control type="title" placeholder="To Kill A Mockingbird" name= "title" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicAuthor">
                 <Form.Label>Author</Form.Label>
-                <Form.Control type="author" placeholder="Harper Lee" />
+                <Form.Control type="author" placeholder="Harper Lee" name = "author" />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicSynopsis">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="synopsis" placeholder="Boo, Scout, and Gregory play in a treehouse" />
+                <Form.Control type="synopsis" placeholder="Boo, Scout, and Gregory play in a treehouse" name="synopsis" />
             </Form.Group>
 
             <Button variant="primary" type="submit">
